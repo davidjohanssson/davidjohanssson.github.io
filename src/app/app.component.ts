4f6eb71bb9ce54fb249271ffb1500cb2029e55a8
David Johansson <david.n.johansson@live.com>
2023-06-24 18:53:17 +0200

    Klar med logik för nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { ProjectsComponent } from "./pages/projects/projects.component";
+import { SkillsComponent } from "./pages/skills/skills.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { filter } from 'rxjs';
+import { NavComponent } from "./components/nav/nav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButtonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        HomeComponent,
+        ProjectsComponent,
+        SkillsComponent,
+        ContactComponent,
+        NavComponent
+    ]
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+	private router = inject(Router);
+	private activatedRoute = inject(ActivatedRoute);
+
+	ngOnInit(): void {
+		this.router.events
+		.pipe(filter(event => event instanceof NavigationEnd))
+		.subscribe(() => {
+			const rt = this.getChild(this.activatedRoute);
+
+			// rt.fragment.subscribe(fragVal => {
+			// 	if (fragVal) {
+			// 		document.querySelector('#' + fragVal)?.scrollIntoView({ behavior: 'smooth' });
+			// 	}
+			// });
+		});
+	}
+
+	private getChild(activatedRoute: ActivatedRoute): ActivatedRoute {
+		if (activatedRoute.firstChild) {
+      return this.getChild(activatedRoute.firstChild);
+    } else {
+      return activatedRoute;
+    }
+	}
 }
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatRippleModule } from '@angular/material/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
+
+interface NavItem {
+  name: string;
+  fragment: string;
+  icon: string;
+  isSelected?: boolean;
+}
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatRippleModule,
+    RouterModule,
+  ],
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  public navItems: NavItem[] = [
+    { name: 'Home', fragment: 'home', icon: 'home' },
+    { name: 'Projects', fragment: 'projects', icon: 'dialogs' },
+    { name: 'Skills', fragment: 'skills', icon: 'tools_power_drill' },
+    { name: 'Contact', fragment: 'contact', icon: 'contact_page' },
+  ];
+
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const fragment = this.activatedRoute.snapshot.fragment;
+        if (fragment === null) return;
+        if (this.navItems.map(navItem => navItem.fragment).includes(fragment) === false) return;
+        const element = document.querySelector('#' + fragment);
+        if (element === null) return;
+        element.scrollIntoView({ behavior: 'smooth' });
+      });
+  }
+
+  private getChild(activatedRoute: ActivatedRoute): ActivatedRoute {
+    if (activatedRoute.firstChild) {
+      return this.getChild(activatedRoute.firstChild);
+    } else {
+      return activatedRoute;
+    }
+  }
+}
