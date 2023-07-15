80ecc427ca0735c156c4d1966d4a7dd00aeb8cc0
David Johansson <david.n.johansson@live.com>
2023-07-15 14:47:18 +0200

    ..

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import React from 'react';
 
 interface Props {
@@ -6,7 +7,26 @@ interface Props {
 
 const Home = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
   return (
-    <div id={props.id} ref={ref} style={{ width: '100%', height: '100vh', backgroundColor: 'green' }}>Home</div>
+    <Box id={props.id} ref={ref} sx={{
+      width: '100%',
+      height: {
+        xs: 'calc(100vh - 48px)',
+        sm: '100vh',
+      },
+      backgroundColor: 'green',
+      display: 'flex',
+      flexDirection: {
+        xs: 'column-reverse',
+        md: 'row',
+      },
+     }}>
+      <Box sx={{ flexGrow: '1' }}>
+        LEFT
+      </Box>
+      <Box sx={{ flexGrow: '1' }}>
+        RIGHT
+      </Box>
+    </Box>
   )
 });
 
