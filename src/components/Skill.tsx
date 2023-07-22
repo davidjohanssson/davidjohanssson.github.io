9b4c667f8b17a126a77f90467ccac487b0ba6bc5
David Johansson <david.n.johansson@live.com>
2023-07-22 17:56:17 +0200

    klar med SkillLegend

---
+++ src/components/Skill.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  name: string;
+}
+
+const Skill: React.FC<Props> = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Skill;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,12 +1,50 @@
+import { Box, Divider, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
+import SkillLegend from './SkillLegend';
 
 interface Props {
   id: string;
 }
 
 const Skills = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const theme = useTheme();
+  const isSmOrUp = useMediaQuery(theme.breakpoints.up('sm'));
+
   return (
-    <div id={props.id} ref={ref} style={{ width: '100%', height: '1024px', backgroundColor: 'yellow' }}>Skills</div>
+    <Box id={props.id} ref={ref} sx={{
+      backgroundColor: 'white',
+      width: '100%',
+      maxWidth: '960px',
+    }}>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: {
+          xs: 'column-reverse',
+          sm: 'row'
+        },
+        justifyContent: {
+          xs: 'center',
+          sm: 'space-evenly',
+        },
+        alignItems: 'center',
+        rowGap: '16px',
+        boxSizing: 'border-box',
+        padding: '32px 16px',
+      }}>
+        <SkillLegend background='#25AE64' name='Beginner' />
+        <Divider variant={isSmOrUp ? 'fullWidth' : 'middle'} orientation={isSmOrUp ? 'vertical' : 'horizontal'} flexItem />
+        <SkillLegend background='#F3C30D' name='Intermediate' />
+        <Divider variant={isSmOrUp ? 'fullWidth' : 'middle'} orientation={isSmOrUp ? 'vertical' : 'horizontal'} flexItem />
+        <SkillLegend background='#E67E22' name='Professional' />
+        <Divider variant={isSmOrUp ? 'fullWidth' : 'middle'} orientation={isSmOrUp ? 'vertical' : 'horizontal'} flexItem />
+        <SkillLegend background='crimson' name='Expert' />
+      </Box>
+      <Box sx={{
+
+      }}>
+        <Box>Programming languages</Box>
+      </Box>
+    </Box>
   )
 });
 
