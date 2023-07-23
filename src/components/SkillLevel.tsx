38959ff3e08c64f5b95df35c129a8893ffd46b5e
David Johansson <david.n.johansson@live.com>
2023-07-23 17:43:57 +0200

    ..

--- src/components/SkillLevel.tsx
+++ src/components/SkillLevel.tsx
@@ -1,3 +1,4 @@
+import { Check } from '@mui/icons-material';
 import { Box, SxProps, Theme } from '@mui/material';
 
 export type Level = 1 | 2 | 3 | 4;
@@ -24,11 +25,18 @@ const SkillLevel: React.FC<Props> = ({ level }) => {
 
   const boxStyle = (boxLevel: Level): SxProps<Theme> => ({
     backgroundColor: getBoxColor(boxLevel, level),
+    color: 'white',
     width: '16px',
     boxSizing: 'border-box',
     border: '1px solid rgba(0, 0, 0, 0.12)',
     borderRadius: '4px',
   });
+
+  const checkStyle: SxProps<Theme> = {
+    fontSize: '17px',
+    marginLeft: '-2px',
+    marginTop: '-1px',
+  };
 
   return (
     <Box sx={{
@@ -38,10 +46,18 @@ const SkillLevel: React.FC<Props> = ({ level }) => {
       justifyContent: 'center',
       columnGap: '8px',
     }}>
-      <Box sx={boxStyle(1)} />
-      <Box sx={boxStyle(2)} />
-      <Box sx={boxStyle(3)} />
-      <Box sx={boxStyle(4)} />
+      <Box sx={boxStyle(1)}>
+        <Check sx={checkStyle} />
+      </Box>
+      <Box sx={boxStyle(2)}>
+        <Check sx={checkStyle} />
+      </Box>
+      <Box sx={boxStyle(3)}>
+        <Check sx={checkStyle} />
+      </Box>
+      <Box sx={boxStyle(4)}>
+        <Check sx={checkStyle} />
+      </Box>
     </Box>
   )
 };
