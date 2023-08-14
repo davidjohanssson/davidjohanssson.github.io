1af9ba642ad8751bdcbddf4735852d3959778bff
David Johansson <david.n.johansson@live.com>
2023-08-14 11:19:58 +0200

    La till färger i temat

--- src/theme.ts
+++ src/theme.ts
@@ -4,4 +4,13 @@ export const theme = createTheme({
   typography: {
     fontFamily: 'Inter, sans-serif',
   },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#344EE2'
+    },
+    secondary: {
+      main: '#00695C',
+    }
+  }
 });
