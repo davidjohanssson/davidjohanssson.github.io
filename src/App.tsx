e3739c2e72f6cfe7496b69679cb9857390c82ed7
David Johansson <david.n.johansson@live.com>
2023-12-10 15:00:29 +0100

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { Box, Paper, Tab, Tabs, ThemeProvider, useTheme } from '@mui/material';
+import { Box, Paper, Tab, Tabs, useTheme } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 import Contact from './components/Contact';
 import Explore from './components/Explore';
-import { theme } from './theme';
 import { useVisibleSection } from './hooks/useVisibleSection';
 
 interface Section {
@@ -11,7 +10,7 @@ interface Section {
 }
 
 function App() {
-  const t = useTheme();
+  const theme = useTheme();
   const [sectionIndex, setSectionIndex] = useState(0);
   const sections = useRef<Section[]>([
     { id: 'Contact', ref: null },
@@ -37,13 +36,15 @@ function App() {
     const updateThemeColorObserver = new IntersectionObserver((entries) => {
       for (const entry of entries) {
         if (entry.isIntersecting) {
-          let backgroundColor = t.palette.primary.light;
+          let background = window.getComputedStyle(entry.target).background;
 
-          if (backgroundColor === 'rgba(0, 0, 0, 0)' || backgroundColor === 'rgb(255, 255, 255)') {
-            backgroundColor = '#F5F5F5';
+          if (background.startsWith('rgb(255, 255, 255)')) {
+            background = '#F5F5F5';
+          } else {
+            background = theme.palette.primary.light;
           }
 
-          document.querySelector('meta[name="theme-color"]')?.setAttribute('content', backgroundColor);
+          document.querySelector('meta[name="theme-color"]')?.setAttribute('content', background);
         }
       }
     }, { root: null, rootMargin: '0px 0px -100% 0px', threshold: 0 });
@@ -53,7 +54,7 @@ function App() {
         updateThemeColorObserver.observe(section.ref);
       }
     }
-  }, [t]);
+  }, [theme.palette.primary.light]);
 
   const handleClick = (section: Section, index: number) => {
     setSectionIndex(index);
@@ -61,74 +62,74 @@ function App() {
     if (section.ref) {
       section.ref.scrollIntoView();
 
-      let color = window.getComputedStyle(section.ref).backgroundColor;
+      let background = window.getComputedStyle(section.ref).background;
 
-      if (color === 'rgba(0, 0, 0, 0)') {
-        color = 'rgb(255, 255, 255)';
+      if (background.startsWith('rgb(255, 255, 255)')) {
+        background = '#F5F5F5';
+      } else {
+        background = theme.palette.primary.light;
       }
 
-      document.querySelector('meta[name="theme-color"]')?.setAttribute('content', color);
+      document.querySelector('meta[name="theme-color"]')?.setAttribute('content', background);
     }
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', position: 'relative', alignItems: 'center' }}>
-        <Box sx={{
-          width: '100%',
-          marginBottom: '48px',
-        }}>
-          {sections.current.map((section, index) => {
-            if (section.id === 'Contact') {
-              return <Contact key={section.id} id={section.id} ref={(ref) => sections.current[index].ref = ref} />
-            } else if (section.id === 'Explore') {
-              return <Explore key={section.id} id={section.id} ref={(ref) => sections.current[index].ref = ref} />
-            } else {
-              return (<div>404 Not Found</div>);
-            }
-          })}
-        </Box>
-        <Paper elevation={6} sx={{
-          backgroundColor: theme.palette.primary.main,
-          color: 'white',
-          position: 'fixed',
-          width: {
-            xs: '100%',
-            sm: 'calc(100% - 32px)',
-          },
-          maxWidth: {
-            xs: 'unset',
-            sm: 'calc(256px + 128px)',
-          },
-          display: 'flex',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-          borderTopLeftRadius: {
-            xs: '0px',
-            sm: '4px',
-          },
-          borderTopRightRadius: {
-            xs: '0px',
-            sm: '4px',
-          },
-          borderBottomLeftRadius: '0px',
-          borderBottomRightRadius: '0px',
-          overflow: 'hidden',
-          bottom: '0px',
-        }}>
-          <Tabs value={sectionIndex} variant='fullWidth' textColor='inherit' sx={{
-            width: '100%',
-            '& .MuiTabs-indicator': {
-              backgroundColor: 'white',
-            },
-          }}>
-            {sections.current.map((section, index) => (
-              <Tab key={section.id} label={section.id} onClick={() => handleClick(section, index)} />
-            ))}
-          </Tabs>
-        </Paper>
+    <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', position: 'relative', alignItems: 'center' }}>
+      <Box sx={{
+        width: '100%',
+        marginBottom: '48px',
+      }}>
+        {sections.current.map((section, index) => {
+          if (section.id === 'Contact') {
+            return <Contact key={section.id} id={section.id} ref={(ref) => sections.current[index].ref = ref} />
+          } else if (section.id === 'Explore') {
+            return <Explore key={section.id} id={section.id} ref={(ref) => sections.current[index].ref = ref} />
+          } else {
+            return (<div>404 Not Found</div>);
+          }
+        })}
       </Box>
-    </ThemeProvider>
+      <Paper elevation={6} sx={{
+        backgroundColor: theme.palette.primary.main,
+        color: 'white',
+        position: 'fixed',
+        width: {
+          xs: '100%',
+          sm: 'calc(100% - 32px)',
+        },
+        maxWidth: {
+          xs: 'unset',
+          sm: 'calc(256px + 128px)',
+        },
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        borderTopLeftRadius: {
+          xs: '0px',
+          sm: '4px',
+        },
+        borderTopRightRadius: {
+          xs: '0px',
+          sm: '4px',
+        },
+        borderBottomLeftRadius: '0px',
+        borderBottomRightRadius: '0px',
+        overflow: 'hidden',
+        bottom: '0px',
+      }}>
+        <Tabs value={sectionIndex} variant='fullWidth' textColor='inherit' sx={{
+          width: '100%',
+          '& .MuiTabs-indicator': {
+            backgroundColor: 'white',
+          },
+        }}>
+          {sections.current.map((section, index) => (
+            <Tab key={section.id} label={section.id} onClick={() => handleClick(section, index)} />
+          ))}
+        </Tabs>
+      </Paper>
+    </Box>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,16 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/pacifico/400.css';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
